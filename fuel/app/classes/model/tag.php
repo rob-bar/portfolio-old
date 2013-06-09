c4c63cbd8a9389d05b020f844cb79713681b6990
Robbie Bardijn <robbie.bardijn@gmail.com>
2013-06-09 15:08:32 +0200

    cleaned repo fresh start

--- fuel/app/classes/model/tag.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Model_Tag extends \Orm\Model {
-	protected static $_properties = array(
-		'id',
-		'title'
-	);
-}
--- fuel/app/classes/model/type.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Model_Type extends \Orm\Model {
-	protected static $_properties = array(
-		'id',
-		'title'
-	);
-}
-?>
