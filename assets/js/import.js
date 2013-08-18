e727bc4a5f5826c4d1f2452018657e5d512d5247
Robbie Bardijn <robbie.bardijn@gmail.com>
2013-08-18 13:19:08 +0200

    added import data & style fixes

---
+++ assets/js/import.js
@@ -0,0 +1,141 @@
+(function() {
+  var project, tags;
+
+  project = {
+    id: 123456789,
+    title: "first project",
+    type: "Website",
+    likes: 10,
+    launch_date: 12 / 12 / 1212,
+    description: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Vivamus vitae erat eu nulla mattis placerat. Nulla consequat interdum dolor bibendum sodales. Fusce at massa sit amet velit posuere sodales vel a lectus. Donec aliquet adipiscing nisl id ullamcorper turpis ornare vitae. Etiam in viverra nisi. Etiam vitae ligula id risus pharetra tristique condimentum ut metus. Praesent auctor massa at sem condimentum suscipit.Nullam ac tincidunt arcu. Sed non tincidunt neque vel sollicitudin orci. Duis scelerisque erat felis id lacinia elit rhoncus nec. Nullam feugiat ipsum a justo malesuada imperdiet. Nam cursus erat tortor a malesuada arcu mollis quis. Donec vestibulum pharetra pharetra. Fusce consectetur turpis eu gravida porttitor. Maecenas sodales vestibulum felis ac tincidunt quam aliquet eu. Quisque quis laoreet est. Pellentesque ullamcorper magna ut eleifend porttitor. Suspendisse euismod sed magna in pulvinar. In in faucibus mauris lobortis semper mi. Pellentesque in nunc feugiat hendrerit nibh vel mollis elit.",
+    is_active: true,
+    media: {
+      thumb: "thumbname.jpg",
+      big: "bignqme.jpg",
+      pictures: true,
+      videos: true,
+      preview: "http://www.google.be"
+    },
+    tags: []
+  };
+
+  project = {
+    id: 123456789,
+    title: "second project",
+    type: "Website",
+    likes: 10,
+    launch_date: 12 / 12 / 1212,
+    description: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Vivamus vitae erat eu nulla mattis placerat. Nulla consequat interdum dolor bibendum sodales. Fusce at massa sit amet velit posuere sodales vel a lectus. Donec aliquet adipiscing nisl id ullamcorper turpis ornare vitae. Etiam in viverra nisi. Etiam vitae ligula id risus pharetra tristique condimentum ut metus. Praesent auctor massa at sem condimentum suscipit.Nullam ac tincidunt arcu. Sed non tincidunt neque vel sollicitudin orci. Duis scelerisque erat felis id lacinia elit rhoncus nec. Nullam feugiat ipsum a justo malesuada imperdiet. Nam cursus erat tortor a malesuada arcu mollis quis. Donec vestibulum pharetra pharetra. Fusce consectetur turpis eu gravida porttitor. Maecenas sodales vestibulum felis ac tincidunt quam aliquet eu. Quisque quis laoreet est. Pellentesque ullamcorper magna ut eleifend porttitor. Suspendisse euismod sed magna in pulvinar. In in faucibus mauris lobortis semper mi. Pellentesque in nunc feugiat hendrerit nibh vel mollis elit.",
+    is_active: true,
+    media: {
+      thumb: "thumbname.jpg",
+      big: "bignqme.jpg",
+      pictures: ["http://placekitten.com/200/300", "http://placekitten.com/200/300"],
+      videos: ["www.youtube.com/embed/2PAT1UmlJ-0"],
+      preview: "http://www.google.be"
+    },
+    tags: []
+  };
+
+  tags = [
+    {
+      name: "canvas",
+      kind: "tech"
+    }, {
+      name: "requirejs",
+      kind: "tech"
+    }, {
+      name: "codeigniter",
+      kind: "tech"
+    }, {
+      name: "fuelphp",
+      kind: "tech"
+    }, {
+      name: "jquery",
+      kind: "tech"
+    }, {
+      name: "backbone",
+      kind: "tech"
+    }, {
+      name: "handlebars",
+      kind: "tech"
+    }, {
+      name: "grunt",
+      kind: "tech"
+    }, {
+      name: "after effects",
+      kind: "tech"
+    }, {
+      name: "node",
+      kind: "tech"
+    }, {
+      name: "socket.io",
+      kind: "tech"
+    }, {
+      name: "flash",
+      kind: "tech"
+    }, {
+      name: "youtube",
+      kind: "api"
+    }, {
+      name: "facebook",
+      kind: "api"
+    }, {
+      name: "google maps",
+      kind: "api"
+    }, {
+      name: "php",
+      kind: "lang"
+    }, {
+      name: "coffeescript",
+      kind: "lang"
+    }, {
+      name: "javascript",
+      kind: "lang"
+    }, {
+      name: "as",
+      kind: "lang"
+    }, {
+      name: "html 5",
+      kind: "lang"
+    }, {
+      name: "css 3",
+      kind: "lang"
+    }, {
+      name: "sass",
+      kind: "lang"
+    }, {
+      name: "less",
+      kind: "lang"
+    }, {
+      name: "3d",
+      kind: "cat"
+    }, {
+      name: "video",
+      kind: "cat"
+    }, {
+      name: "responsive site",
+      kind: "cat"
+    }, {
+      name: "mobile site",
+      kind: "cat"
+    }, {
+      name: "website",
+      kind: "cat"
+    }, {
+      name: "landing page",
+      kind: "cat"
+    }, {
+      name: "b2b website",
+      kind: "cat"
+    }, {
+      name: "game",
+      kind: "cat"
+    }
+  ];
+
+}).call(this);
+
+/*
+//@ sourceMappingURL=import.js.map
+*/
