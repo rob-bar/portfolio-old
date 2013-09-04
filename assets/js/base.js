aaaa40b88a3894b58328bfbe170bdba96a04af92
Robbie Bardijn <robbie.bardijn@gmail.com>
2013-09-04 10:39:36 +0200

    created cv link on the front end

--- assets/js/base.js
+++ assets/js/base.js
@@ -9,9 +9,27 @@
         return $(repo).addClass("smaller");
       }
     });
-    return $('.likes').click(function(e) {
+    $('.likes').click(function(e) {
       e.preventDefault();
       return $(e.currentTarget).addClass("liked");
+    });
+    return $('.work').click(function(e) {
+      e.preventDefault();
+      if (!$('#all li').eq(0).hasClass("inback")) {
+        return $('#all li').each(function() {
+          var _this = this;
+          return setTimeout(function() {
+            return $(_this).addClass("inback");
+          }, $(this).offset().top * 0.75);
+        });
+      } else {
+        return $('#all li').each(function() {
+          var _this = this;
+          return setTimeout(function() {
+            return $(_this).removeClass("inback");
+          }, $(this).offset().top * 0.75);
+        });
+      }
     });
   });
 
